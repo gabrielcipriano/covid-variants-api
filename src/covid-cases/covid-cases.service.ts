3906b73e0fe345b813165a2a4873438136ade32b
Gabriel Cipriano <gabrielfcipriano@gmail.com>
2022-04-19 14:46:09 -0300

    service layer implementation

--- src/covid-cases/covid-cases.service.ts
+++ src/covid-cases/covid-cases.service.ts
@@ -2,8 +2,20 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CovidCase } from './covid-cases.entity';
-import { LocationVariantsDto } from './dtos/location-variants.dto';
 import { ValidDateStr } from './types/valid-date-str.type';
+
+import { LocationVariantsRecord } from './types/location-variants-record.type';
+
+function groupResultsByLocation(
+  queryResult: CovidCase[],
+): LocationVariantsRecord {
+  return queryResult.reduce((record: LocationVariantsRecord, currentRow) => {
+    const { location, variant, num_sequences } = currentRow;
+    if (!record[location]) record[location] = {};
+    record[location][variant] = num_sequences;
+    return record;
+  }, {});
+}
 
 @Injectable()
 export class CovidCasesService {
@@ -12,21 +24,23 @@ export class CovidCasesService {
     private readonly repository: Repository<CovidCase>,
   ) {}
 
-  async countForDate(date: ValidDateStr): Promise<Array<LocationVariantsDto>> {
-    const queryResult = this.repository
-      .createQueryBuilder('covid_case')
-      .where('covid_case.date = :date', { date })
-      .getMany();
-    return queryResult as unknown as Array<LocationVariantsDto>;
+  async countForDate(date: ValidDateStr): Promise<LocationVariantsRecord> {
+    const queryResult = await this.repository.find({ date });
+
+    return groupResultsByLocation(queryResult);
   }
 
   async cumulativeForDate(
-    date: ValidDateStr,
-  ): Promise<Array<LocationVariantsDto>> {
-    const queryResult = this.repository
+    _date: ValidDateStr,
+  ): Promise<LocationVariantsRecord> {
+    const queryResult = await this.repository
       .createQueryBuilder('covid_case')
-      .where('covid_case.date = :date', { date })
-      .getMany();
-    return queryResult as unknown as Array<LocationVariantsDto>;
+      .select('location, variant, sum(num_sequences) as num_sequences')
+      .where('covid_case.date <= :date', { date: _date })
+      .groupBy('covid_case.location')
+      .addGroupBy('covid_case.variant')
+      .getRawMany<CovidCase>();
+
+    return groupResultsByLocation(queryResult);
   }
 }
