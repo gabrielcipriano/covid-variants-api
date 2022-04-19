f35a400dcfb8ced131dc521076e7630b699bc8d7
Gabriel Cipriano <gabrielfcipriano@gmail.com>
2022-04-19 14:45:56 -0300

    service layers tests

--- src/covid-cases/covid-cases.service.spec.ts
+++ src/covid-cases/covid-cases.service.spec.ts
@@ -1,46 +1,88 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { getRepositoryToken } from '@nestjs/typeorm';
+import { getRepositoryToken, TypeOrmModule } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CovidCase } from './covid-cases.entity';
 import { CovidCasesService } from './covid-cases.service';
-import { LocationVariantsDto } from './dtos/location-variants.dto';
-import { VariantSequencesDto } from './dtos/variant-sequences.dto';
 import { ValidDateStr } from './types/valid-date-str.type';
+import { LocationVariantsRecord } from './types/location-variants-record.type';
 
 const DATE = '2022-04-02' as ValidDateStr;
 
-const VARIANT1 = new VariantSequencesDto('Alpha', 100);
-
-const LOCATION1 = new LocationVariantsDto('Brazil', DATE);
-LOCATION1.variants.push(VARIANT1);
-
-const location1_cumulated = {
-  ...LOCATION1,
-  variants: [
-    new VariantSequencesDto(VARIANT1.variant, VARIANT1.num_sequences + 50),
+const cumulativeResultQueryBuilder = {
+  select: () => cumulativeResultQueryBuilder,
+  where: () => cumulativeResultQueryBuilder,
+  groupBy: () => cumulativeResultQueryBuilder,
+  addGroupBy: () => cumulativeResultQueryBuilder,
+  getMany: () => [
+    {
+      location: 'Brazil',
+      variant: 'Delta',
+      sum: 150,
+    },
+    {
+      location: 'Brazil',
+      variant: 'Omicron',
+      sum: 50,
+    },
+    {
+      location: 'Italy',
+      variant: 'Omicron',
+      sum: 200,
+    },
   ],
 };
 
 describe('CovidCasesService', () => {
   let service: CovidCasesService;
   let repo: Repository<CovidCase>;
-  let spy_querybuilder;
+  // let spy_querybuilder;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      // imports:[getRepositoryToken(CovidCase)]
       providers: [
         CovidCasesService,
         {
           provide: getRepositoryToken(CovidCase),
-          useClass: Repository,
+          // useClass: Repository,
+          useValue: {
+            find: jest
+              .fn()
+              .mockImplementation(async ({}: any) => [
+                new CovidCase('Brazil', '2022-04-02', 'Delta', 100),
+                new CovidCase('Brazil', '2022-04-02', 'Omicron', 500),
+                new CovidCase('Italy', '2022-04-02', 'Delta', 200),
+              ]),
+            createQueryBuilder: jest.fn(() => ({
+              select: jest.fn().mockReturnThis(),
+              where: jest.fn().mockReturnThis(),
+              groupBy: jest.fn().mockReturnThis(),
+              addGroupBy: jest.fn().mockReturnThis(),
+              getRawMany: jest.fn().mockReturnValueOnce([
+                {
+                  location: 'Brazil',
+                  variant: 'Delta',
+                  num_sequences: 150,
+                },
+                {
+                  location: 'Brazil',
+                  variant: 'Omicron',
+                  num_sequences: 500,
+                },
+                {
+                  location: 'Italy',
+                  variant: 'Omicron',
+                  num_sequences: 200,
+                },
+              ]),
+            })),
+          },
         },
       ],
     }).compile();
 
     service = module.get<CovidCasesService>(CovidCasesService);
     repo = module.get<Repository<CovidCase>>(getRepositoryToken(CovidCase));
-    spy_querybuilder = jest.spyOn(repo, 'createQueryBuilder');
+    // spy_querybuilder = jest.spyOn(repo, 'createQueryBuilder');
   });
 
   it('should be defined', async () => {
@@ -48,15 +90,8 @@ describe('CovidCasesService', () => {
   });
 
   it('should call the repository', async () => {
-    const createQueryBuilder: any = () => ({
-      where: createQueryBuilder,
-      getMany: createQueryBuilder,
-    });
-
-    spy_querybuilder.mockImplementation(createQueryBuilder);
-
     await service.countForDate(DATE);
-    expect(repo.createQueryBuilder).toHaveBeenCalled();
+    expect(repo.find).toHaveBeenCalled();
   });
 
   describe('countForDate', () => {
@@ -64,27 +99,23 @@ describe('CovidCasesService', () => {
       expect(service.countForDate).toBeDefined();
     });
 
-    it('should call the query chain', async () => {
-      const createQueryBuilder: any = () => ({
-        where: createQueryBuilder,
-        getMany: createQueryBuilder,
-      });
-
-      spy_querybuilder.mockImplementation(createQueryBuilder);
-
+    it('should call repository`s find with a date.', async () => {
       service.countForDate(DATE);
-      expect(repo.createQueryBuilder).toHaveBeenCalled();
+      expect(repo.find).toHaveBeenCalledWith({ date: DATE });
     });
 
-    it('should return a list of results for a given date', () => {
-      const createQueryBuilder: any = () => ({
-        where: createQueryBuilder,
-        getMany: () => [LOCATION1],
-      });
+    it('should return results grouped by location for a given date', async () => {
+      const expectedResult: LocationVariantsRecord = {
+        Brazil: {
+          Delta: 100,
+          Omicron: 500,
+        },
+        Italy: {
+          Delta: 200,
+        },
+      };
 
-      spy_querybuilder.mockImplementation(createQueryBuilder);
-
-      expect(service.countForDate(DATE)).resolves.toEqual([LOCATION1]);
+      expect(service.countForDate(DATE)).resolves.toEqual(expectedResult);
     });
   });
 
@@ -94,36 +125,22 @@ describe('CovidCasesService', () => {
     });
 
     it('should call the query chain', async () => {
-      const createQueryBuilder: any = () => ({
-        where: createQueryBuilder,
-        getMany: createQueryBuilder,
-      });
-
-      spy_querybuilder.mockImplementation(createQueryBuilder);
-
-      service.cumulativeForDate(DATE);
+      await service.cumulativeForDate(DATE);
       expect(repo.createQueryBuilder).toHaveBeenCalled();
     });
 
-    it('should return a list of results for a given date', () => {
-      const createQueryBuilder: any = () => ({
-        where: createQueryBuilder,
-        getMany: () => {
-          const variant = new VariantSequencesDto('Alpha', 100);
-
-          const location = new LocationVariantsDto('Brazil', DATE);
-          location.variants.push(variant);
-          const accumulated_value = 50;
-          location.variants[0].num_sequences += accumulated_value;
-
-          return [location];
+    it('should return results grouped by location for a given date', () => {
+      const expectedResult: LocationVariantsRecord = {
+        Brazil: {
+          Delta: 150,
+          Omicron: 500,
         },
-      });
+        Italy: {
+          Omicron: 200,
+        },
+      };
 
-      spy_querybuilder.mockImplementation(createQueryBuilder);
-      expect(service.cumulativeForDate(DATE)).resolves.toEqual([
-        location1_cumulated,
-      ]);
+      expect(service.cumulativeForDate(DATE)).resolves.toEqual(expectedResult);
     });
   });
 });
--- src/covid-cases/dtos/variant-sequences.dto.ts
+++
@@ -1,9 +0,0 @@
-export class VariantSequencesDto {
-  variant: string;
-  num_sequences: number;
-
-  constructor(variant: string, num_sequences: number) {
-    this.variant = variant;
-    this.num_sequences = num_sequences;
-  }
-}
